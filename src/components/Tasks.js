27097c5934bb679415f1dc13ba47f5bba068e280
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-11 12:58:29 +0100

    added task removal

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Task from "./Task";
 
-const Tasks = ({ tasks }) => {
+
+const Tasks = ({ tasks, onDelete }) => {
     return (
         <React.Fragment>
             {tasks.map((task) => (
-                <h3 key={task.id}>{task.text}</h3>
+                <Task key={task.id} task={task} onDelete={onDelete}/>
             ))}
         </React.Fragment>
     );
