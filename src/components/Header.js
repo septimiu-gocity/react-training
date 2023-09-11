300d3cf57a2b281fc285daa031058c0385ec500b
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-11 11:47:03 +0100

    setup the index.css file and added a button

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+const Header = ({title}) => {
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <button className="btn">Add</button>
+        </header>
+    );
+};
+
+Header.defaultProps = {
+    title: "task tracker",
+};
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+};
+
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+
+export default Header;
