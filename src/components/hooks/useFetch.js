5b4687c468d5299adb33d92729bd6eb76ca59cdb
Septimiu-Iulian Calin <septimiu.calin@gocity.com>
2023-09-12 10:22:42 +0100

    small change in var name

--- src/components/hooks/useFetch.js
+++ src/components/hooks/useFetch.js
@@ -9,17 +9,17 @@ const useFetch = (requestConfig) => {
         setIsLoading(true);
         const fetchData = async () => {
             try {
-                const response = await fetch(requestConfig.url, {
+                const res = await fetch(requestConfig.url, {
                     method: requestConfig.method,
                     headers: requestConfig.headers,
                     body: JSON.stringify(requestConfig.body),
                 });
 
-                if (!response.ok) {
+                if (!res.ok) {
                     throw new Error("Request failed");
                 }
 
-                const data = await response.json();
+                const data = await res.json();
                 setData(data);
             } catch (err) {
                 setError(err.message || "There is an error");
